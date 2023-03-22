a03e1cc2999079533630fe21be0d08ce63d7461a
saman <s.saman@iitg.ac.in>
2023-03-22 18:08:14 +0530

    second task

--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,17 @@ import { getTable } from "../controllers/getTable.js";
 import { addInTable } from "../controllers/addInTable.js";
 import { updateTable } from "../controllers/updateTable.js";
 import { deleteInTable } from "../controllers/deleteInTable.js";
+import { sendMailAPI } from "../controllers/sendMailAPI.js";
 
 const router = express.Router();
 
-
+// airtable
 router.get("/getTable", getTable);
 router.post("/addInTable", addInTable);
 router.post("/updateTable", updateTable);
 router.delete("/deleteInTable", deleteInTable);
+
+// send mail
+router.get("/sendMailAPI", sendMailAPI);
 
 export default router;
