35337f1e44c535cdfad2fe5e19431f1645b4e9cf
saman <s.saman@iitg.ac.in>
2023-04-11 14:38:35 +0530

    .

--- controllers/excel/excelRead.js
+++ controllers/excel/excelRead.js
@@ -8,35 +8,36 @@ const REDIRECT_URI = process.env.REDIRECT_URI;
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN_SHEET;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN_SHEET;
 
+
+
 export const excelRead = async (req, res) => {
     try {
         const workbook = new Excel.Workbook();
-        let values = [];
-        workbook.xlsx.readFile('./files/excelTask.xlsx')
-            .then(function () {
-                const worksheet = workbook.getWorksheet('Sheet1');
-                worksheet.eachRow(function (row, rowNumber) {
-                    const col1 = row.getCell('A').value;
-                    const col2 = row.getCell('B').value;
-                    const col3 = row.getCell('C').value;
-                    const col4 = row.getCell('D').value;
-                    const col5 = row.getCell('E').value;
-                    const col6 = row.getCell('F').value;
-                    const col7 = row.getCell('G').value;
-                    const col8 = row.getCell('H').value;
-                    const col9 = row.getCell('I').value;
-                    const col10 = row.getCell('J').value;
-                    const col11 = row.getCell('K').value;
-                    const col12 = row.getCell('L').value;
-                    const col13 = row.getCell('M').value;
-                    const col14 = row.getCell('N').value;
-                    let thisrow = [col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14];
-                    console.log(thisrow);
-                    values.push(thisrow);
-                });
-            });
+        await workbook.xlsx.readFile('./files/excelTask.xlsx');
 
-        console.log(values)
+        const sheetName = 'Sheet1';
+        const worksheet = workbook.getWorksheet(sheetName);
+
+        let values = [];
+        worksheet.eachRow((row, rowNumber) => {
+            const col1 = row.getCell('A').value;
+            const col2 = row.getCell('B').value;
+            const col3 = row.getCell('C').value;
+            const col4 = row.getCell('D').value;
+            const col5 = row.getCell('E').value;
+            const col6 = row.getCell('F').value;
+            const col7 = row.getCell('G').value;
+            const col8 = row.getCell('H').value;
+            const col9 = row.getCell('I').value;
+            const col10 = row.getCell('J').value;
+            const col11 = row.getCell('K').value;
+            const col12 = row.getCell('L').value;
+            const col13 = row.getCell('M').value;
+            const col14 = row.getCell('N').value;
+            let thisrow = [col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14];
+            values.push(thisrow);
+        });
+
 
         const auth = new google.auth.OAuth2(
             CLIENT_ID,
@@ -62,16 +63,15 @@ export const excelRead = async (req, res) => {
             resource: {
                 values: newValue,
             },
-        }, (err, res) => {
+        }, (err, res1) => {
             if (err) {
-                console.log(`The API returned an error: ${err}`);
+                res.status(400).json({ success: false, message: err });
                 return;
             }
-            console.log(res.data);
+            console.log(res1.data);
+            res.status(200).json({ success: true, message: "Data extracted" })
         });
 
-
-        res.status(200).json({ success: true, message: "Data extracted" })
     }
     catch (error) {
         res.status(500).json({ success: false, message: error.message });
