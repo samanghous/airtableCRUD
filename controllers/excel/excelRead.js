735a62cb330953ab853af4a421a1094a9cd05bb5
saman <s.saman@iitg.ac.in>
2023-04-11 14:13:04 +0530

    .

--- controllers/excel/excelRead.js
+++ controllers/excel/excelRead.js
@@ -2,16 +2,16 @@ import Excel from 'exceljs';
 import fs from 'fs';
 import { google } from 'googleapis';
 
-const CLIENT_ID = process.env.CLIENT_ID ;
-const CLIENT_SECRET = process.env.CLIENT_SECRET ;
-const REDIRECT_URI = process.env.REDIRECT_URI ;
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN_SHEET ;
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN_SHEET ;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN_SHEET;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN_SHEET;
 
 export const excelRead = async (req, res) => {
     try {
         const workbook = new Excel.Workbook();
-        let values = []
+        let values = [];
         workbook.xlsx.readFile('./files/excelTask.xlsx')
             .then(function () {
                 const worksheet = workbook.getWorksheet('Sheet1');
@@ -30,10 +30,13 @@ export const excelRead = async (req, res) => {
                     const col12 = row.getCell('L').value;
                     const col13 = row.getCell('M').value;
                     const col14 = row.getCell('N').value;
-                    values.push([col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14]);
+                    let thisrow = [col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14];
+                    console.log(thisrow);
+                    values.push(thisrow);
                 });
             });
 
+        console.log(values)
 
         const auth = new google.auth.OAuth2(
             CLIENT_ID,
